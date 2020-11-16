05a16a4deba3a25885ddbc7e724d58b27422b61a
lerriuqs <631253076@QQ.COM>
2020-11-16 22:06:26 +0800

    leetcode 633

---
+++ 633. Sum of Square Numbers/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean judgeSquareSum(int c) {
+        int l = 0;
+        int r = (int) Math.sqrt(c);
+        int sum;
+        while (l<=r) {
+            sum = l * l + r * r;
+            if (sum == c) {
+                return true;
+            } else if (sum < c) {
+                l += 1;
+            } else {
+                r -= 1;
+            }
+        }
+        return false;
+    }
+}
