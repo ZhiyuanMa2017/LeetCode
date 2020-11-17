476ccf3c0649087da52c245fe3f97ddbe607c4b8
lerriuqs <631253076@QQ.COM>
2020-11-17 22:32:10 +0800

    leetcode 69

---
+++ 69. Sqrt(x)/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public static int mySqrt(int x) {
+        if (x <= 1) {
+            return x;
+        }
+        int l = 0;
+        int h = x;
+        while(l <= h) {
+            int mid = l + (h-l) / 2;
+
+            if (mid == x / mid) {
+                return mid;
+            } else if (mid > x/mid) {
+                h = mid - 1;
+            } else if (mid < x/mid){
+                l = mid + 1;
+            }
+        }
+        return h;
+    }
+}
