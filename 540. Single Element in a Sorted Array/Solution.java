2dfb313de5b9cbd19306955371629a4ca3c79e7f
lerriuqs <631253076@QQ.COM>
2020-11-18 21:15:10 +0800

    leetcode 540

---
+++ 540. Single Element in a Sorted Array/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int l = 0;
+        int h = nums.length - 1;
+        while (l < h) {
+            int mid = l + (h - l) / 2;
+            if (mid % 2 != 0) {
+                mid -= 1;
+            }
+            if (nums[mid] != nums[mid + 1]){
+                h = mid;
+            } else {
+                l = mid + 2;
+            }
+        }
+        return nums[l];
+    }
+}
