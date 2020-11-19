7782030ac0922d2eb5f35e63003cb9ba1db462ec
lerriuqs <631253076@QQ.COM>
2020-11-19 17:07:40 +0800

    leetcode 83

---
+++ 83. Remove Duplicates from Sorted List/Solution.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode curr = head;
+        while (curr != null) {
+            if (curr.next != null && curr.val == curr.next.val) {
+                curr.next = curr.next.next;
+            } else {
+                curr = curr.next;
+            }
+        }
+        return head;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
---
+++ 83. Remove Duplicates from Sorted List/Solution2.java
@@ -0,0 +1,13 @@
+class Solution2 {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        head.next = deleteDuplicates(head.next);
+        if (head.val == head.next.val) {
+            return head.next;
+        } else {
+            return head;
+        }
+    }
+}
