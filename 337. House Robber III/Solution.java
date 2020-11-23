6e8630a3467efee93d966f3cb3262b5f9ef12840
lerriuqs <631253076@QQ.COM>
2020-11-23 18:52:27 +0800

    leetcode 337

---
+++ 337. House Robber III/Solution.java
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    public int rob(TreeNode root) {
+        int[] res = robhelp(root);
+        return Math.max(res[0], res[1]);
+    }
+    public int[] robhelp(TreeNode root) {
+        if (root == null) {
+            return new int[2];
+        }
+        int[] left = robhelp(root.left);
+        int[] right = robhelp(root.right);
+        int[] res = new int[2];
+        // res[0] exclude self
+        // res[1] include self
+        res[0] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+        res[1] = left[0] + right[0] + root.val;
+        return res;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
