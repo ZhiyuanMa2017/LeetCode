0e9065bc22ecce85b74bb72064f9d9e814a1fd01
lerriuqs <631253076@QQ.COM>
2020-11-24 19:42:05 +0800

    leetcode 227

---
+++ 227. Basic Calculator II/Solution.java
@@ -0,0 +1,35 @@
+class Solution {
+    public static int calculate(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        int len = s.length();
+        int prevnum = 0;
+        int currnum = 0;
+        int res = 0;
+        char op = '+';
+        for (int i = 0; i < len; i++) {
+            char currchar = s.charAt(i);
+            if (Character.isDigit(currchar)) {
+                currnum = currnum * 10 + (currchar - '0');
+            }
+            if (!Character.isDigit(currchar) && !Character.isWhitespace(currchar)|| i == len - 1) {
+                if (op == '+') {
+                    res += prevnum;
+                    prevnum = currnum;
+                } else if (op == '-') {
+                    res += prevnum;
+                    prevnum = -currnum;
+                } else if (op == '*') {
+                    prevnum = prevnum * currnum;
+                } else if (op == '/') {
+                    prevnum = prevnum / currnum;
+                }
+                op = currchar;
+                currnum = 0;
+            }
+        }
+        res += prevnum;
+        return res;
+    }
+}
