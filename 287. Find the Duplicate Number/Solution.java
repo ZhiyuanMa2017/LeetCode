8449874871e74009a7d0d1a926e0b8341d8b1f67
lerriuqs <631253076@QQ.COM>
2020-11-25 23:37:41 +0800

    leetcode 287

---
+++ 287. Find the Duplicate Number/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int slow = nums[0];
+        int fast = nums[nums[0]];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+        int slow2 = 0;
+        while (slow != slow2) {
+            slow = nums[slow];
+            slow2 = nums[slow2];
+        }
+        return slow;
+    }
+}
