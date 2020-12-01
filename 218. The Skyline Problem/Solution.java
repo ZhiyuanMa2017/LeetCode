ea7201fb1ef88cb61d498f960cc9ac4a8f29bac3
lerriuqs <631253076@QQ.COM>
2020-12-01 17:00:40 +0800

    leetcode 218

---
+++ 218. The Skyline Problem/Solution.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> getSkyline(int[][] buildings) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<int[]> height = new ArrayList<>();
+        for (int[] b : buildings) {
+            height.add(new int[]{b[0], -b[2]});
+            height.add(new int[]{b[1], b[2]});
+        }
+        height.sort((a, b) -> (a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]));
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> (b - a));
+        pq.offer(0);
+        int premax = 0;
+        for (int[] h : height) {
+            if (h[1] < 0) {
+                pq.offer(-h[1]);
+            } else {
+                pq.remove(h[1]);
+            }
+            int curmax = pq.peek();
+            if (curmax != premax) {
+                List<Integer> tmp = new ArrayList<>(Arrays.asList(h[0], curmax));
+                res.add(tmp);
+                premax = curmax;
+            }
+        }
+        return res;
+    }
+}
