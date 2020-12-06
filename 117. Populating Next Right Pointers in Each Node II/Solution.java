1e658e200c1410aefb7ea603ce29366aa7b05518
lerriuqs <631253076@QQ.COM>
2020-12-06 15:22:55 +0800

    leetcode 117

---
+++ 117. Populating Next Right Pointers in Each Node II/Solution.java
@@ -0,0 +1,70 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+        if (root == null) {
+            return null;
+        }
+        Node sentinel = new Node(0);
+        Node cur = root;
+        while (cur != null) {
+            Node pre = sentinel;
+            while (cur != null) {
+                if (cur.left != null) {
+                    pre.next = cur.left;
+                    pre = pre.next;
+                }
+                if (cur.right != null) {
+                    pre.next = cur.right;
+                    pre = pre.next;
+                }
+                cur = cur.next;
+            }
+            cur = sentinel.next;
+            sentinel.next = null;
+        }
+        return root;
+    }
+}
+
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {
+    }
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+}
