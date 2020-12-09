8f54b41f3e884ce5c8c79444ad661dd6368716a6
lerriuqs <631253076@QQ.COM>
2020-12-09 17:02:19 +0800

    leetcode 173

---
+++ 173. Binary Search Tree Iterator/Solution.java
@@ -0,0 +1,66 @@
+import edu.princeton.cs.algs4.Stack;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class BSTIterator {
+    private TreeNode visit;
+    private Stack<TreeNode> stack;
+
+    public BSTIterator(TreeNode root) {
+        visit = root;
+        stack = new Stack<>();
+    }
+
+    public int next() {
+        while (visit != null) {
+            stack.push(visit);
+            visit = visit.left;
+        }
+        TreeNode next = stack.pop();
+        visit = next.right;
+        return next.val;
+    }
+
+    public boolean hasNext() {
+        return visit != null || !stack.isEmpty();
+    }
+}
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator obj = new BSTIterator(root);
+ * int param_1 = obj.next();
+ * boolean param_2 = obj.hasNext();
+ */
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
