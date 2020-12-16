99d0bce1106db690654fcfd52205eec66319c91d
Zhiyuan Ma <631253076@qq.com>
2020-12-16 22:23:00 +0800

    leetcode 977

---
+++ 977. Squares of a Sorted Array/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int n = nums.length;
+        int l = 0;
+        int h = n - 1;
+        int p = n - 1;
+        int[] res = new int[n];
+        while (l < h) {
+            if (Math.abs(nums[l]) >= Math.abs(nums[h])) {
+                res[p] = nums[l] * nums[l];
+                l++;
+                p--;
+            } else {
+                res[p] = nums[h] * nums[h];
+                h--;
+                p--;
+            }
+        }
+        return res;
+    }
+}
