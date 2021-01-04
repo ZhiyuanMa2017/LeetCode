6e408a6c01a4408823dc3c4eca12e9fd2459ddb4
Zhiyuan Ma <631253076@qq.com>
2021-01-04 16:22:30 +0800

    leetcode 21

--- 21. Merge Two Sorted Lists/Solution.java
+++ 21. Merge Two Sorted Lists/Solution.java
@@ -31,19 +31,3 @@ class Solution {
     }
 }
 
-class ListNode {
-    int val;
-    ListNode next;
-
-    ListNode() {
-    }
-
-    ListNode(int val) {
-        this.val = val;
-    }
-
-    ListNode(int val, ListNode next) {
-        this.val = val;
-        this.next = next;
-    }
-}
