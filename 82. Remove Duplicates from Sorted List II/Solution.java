7922795ba37b6b727b1117fee99b740a25a5840a
lerriuqs <631253076@QQ.COM>
2021-01-05 22:13:02 +0800

    leetcode 82

---
+++ 82. Remove Duplicates from Sorted List II/Solution.java
@@ -0,0 +1,47 @@
+class Solution {
+    public static ListNode deleteDuplicates(ListNode head) {
+        ListNode sentinel = new ListNode(-200);
+        sentinel.next = head;
+        ListNode curr = head;
+        ListNode prev = sentinel;
+        ListNode prevprev = new ListNode(-300);
+        prevprev.next = sentinel;
+        while (curr != null) {
+            if (prev.val != curr.val) {
+                prevprev = prevprev.next;
+                prev = prev.next;
+                curr = curr.next;
+            } else {
+                while (prev.val == curr.val) {
+                    curr = curr.next;
+                    if (curr == null) {
+                        break;
+                    }
+                }
+                prevprev.next = curr;
+                if (curr != null) {
+                    prev = curr;
+                    curr = curr.next;
+                }
+            }
+        }
+        return sentinel.next;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
