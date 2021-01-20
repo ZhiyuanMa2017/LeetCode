117a9990ae0252a368794d65f04a296c59ec47c9
Zhiyuan Ma <631253076@qq.com>
2021-01-20 20:57:07 +0800

    leetcode 20

---
+++ 20. Valid Parentheses/Solution.java
@@ -0,0 +1,19 @@
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> ss = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                ss.add(')');
+            } else if (c == '{') {
+                ss.add('}');
+            } else if (c == '[') {
+                ss.add(']');
+            } else if (ss.isEmpty() || ss.pop() != c){
+                return false;
+            }
+        }
+        return ss.isEmpty();
+    }
+}
