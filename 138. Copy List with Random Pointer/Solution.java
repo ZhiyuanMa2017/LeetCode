174f77a1469ee8c0420326d4dd1e99837e64acd6
lerriuqs <631253076@QQ.COM>
2021-02-11 13:10:31 +0800

    leetcode 138

---
+++ 138. Copy List with Random Pointer/Solution.java
@@ -0,0 +1,51 @@
+class Solution {
+    public Node copyRandomList(Node head) {
+        Node cur = head;
+        Node next;
+        while (cur != null) {
+            next = cur.next;
+            Node copy = new Node(cur.val);
+            cur.next = copy;
+            copy.next = next;
+
+            cur = next;
+        }
+
+        cur = head;
+        while (cur != null) {
+            if (cur.random != null) {
+                cur.next.random = cur.random.next;
+            }
+            cur = cur.next.next;
+        }
+
+        cur = head;
+        Node sentinel = new Node(0);
+        Node res = sentinel;
+        Node resnext;
+        while (cur != null) {
+            next = cur.next.next;
+
+            resnext = cur.next;
+            res.next = resnext;
+            res = resnext;
+
+            cur.next = next;
+            cur = next;
+
+        }
+        return sentinel.next;
+    }
+}
+
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
