ceddd26034a7cd6ddf4da1b0c48c5ba08874906a
Zhiyuan Ma <631253076@qq.com>
2021-02-19 13:07:54 +0800

    leetcode 413

---
+++ 413. Arithmetic Slices/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numberOfArithmeticSlices(int[] A) {
+        int cur = 0;
+        int sum = 0;
+        for (int i = 2; i < A.length; i++) {
+            if (A[i] - A[i - 1] == A[i - 1] - A[i - 2]) {
+                cur++;
+                sum += cur;
+            } else {
+                cur = 0;
+            }
+        }
+        return sum;
+    }
+}
