028f9b097a2c734348e039c083bb917cc7e69759
Zhiyuan Ma <631253076@qq.com>
2021-03-03 17:33:54 +0800

    leetcode 268

---
+++ 268. Missing Number/Solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            res += i + 1 - nums[i];
+        }
+        return res;
+    }
+}
