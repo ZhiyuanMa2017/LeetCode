808a7d587d8663f365fab0ca8eb55bc44f53d702
Zhiyuan Ma <631253076@qq.com>
2021-03-10 03:04:30 +0800

    leetcode 623

--- 623. Add One Row to Tree/Solution.java
+++ 623. Add One Row to Tree/Solution.java
@@ -12,23 +12,23 @@ class Solution {
         }
         return root;
     }
-}
 
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
 
-    TreeNode() {
-    }
+        TreeNode() {
+        }
 
-    TreeNode(int val) {
-        this.val = val;
-    }
+        TreeNode(int val) {
+            this.val = val;
+        }
 
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
     }
 }
