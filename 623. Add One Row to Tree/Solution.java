26017441eeb0fbf118fef84a57cb10cf5558950a
Zhiyuan Ma <zhiyuanma@MacBookPro.local>
2021-03-10 02:51:09 +0800

    leetcode 623

---
+++ 623. Add One Row to Tree/Solution.java
@@ -0,0 +1,34 @@
+class Solution {
+    public TreeNode addOneRow(TreeNode root, int v, int d) {
+        if (d == 0 || d == 1) {
+            TreeNode newroot = new TreeNode(v);
+            newroot.left = d == 1 ? root : null;
+            newroot.right = d == 0 ? root : null;
+            return newroot;
+        }
+        if (root != null && d >= 2) {
+            root.left = addOneRow(root.left, v, d > 2 ? d - 1 : 1);
+            root.right = addOneRow(root.right, v, d > 2 ? d - 1 : 0);
+        }
+        return root;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
