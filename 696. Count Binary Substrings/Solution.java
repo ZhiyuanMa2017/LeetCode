f86a29575a8c8d0476f8fcab2b3dbbe4d968825b
Zhiyuan Ma <631253076@qq.com>
2021-04-24 19:14:25 +0800

    leetcode 696

---
+++ 696. Count Binary Substrings/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int countBinarySubstrings(String s) {
+        int cur = 1;
+        int pre = 0;
+        int res = 0;
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == s.charAt(i - 1)) {
+                cur++;
+            } else {
+                res += Math.min(cur, pre);
+                pre = cur;
+                cur = 1;
+            }
+        }
+        return res + Math.min(cur, pre);
+    }
+}
