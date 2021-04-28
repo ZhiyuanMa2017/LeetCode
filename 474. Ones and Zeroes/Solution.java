61e6b6d17d67fa2f8cda5ebf376beb204305d6ba
Zhiyuan Ma <631253076@qq.com>
2021-04-28 20:26:05 +0800

    leetcode 474

---
+++ 474. Ones and Zeroes/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findMaxForm(String[] strs, int m, int n) {
+        int[][] dp = new int[m + 1][n + 1];
+        for (String str : strs) {
+            int[] count = new int[2];
+            for (char c : str.toCharArray()) {
+                count[c - '0']++;
+            }
+            for (int i = m; i >= count[0]; i--) {
+                for (int j = n; j >= count[1]; j--) {
+                    dp[i][j] = Math.max(dp[i][j], dp[i - count[0]][j - count[1]] + 1);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
