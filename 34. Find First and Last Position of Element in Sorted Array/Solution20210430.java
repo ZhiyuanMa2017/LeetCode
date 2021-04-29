fb52276645dab35d6f86887ecd4d887ac4b4c4d9
Zhiyuan Ma <631253076@qq.com>
2021-04-30 04:53:09 +0800

    leetcode 34

--- 34. Find First and Last Position of Element in Sorted Array/Solution20210430.java
+++ 34. Find First and Last Position of Element in Sorted Array/Solution20210430.java
@@ -1,24 +1,22 @@
-class Solution {
+public class Solution20210430 {
     public int[] searchRange(int[] nums, int target) {
         if (nums.length == 0) {
             return new int[]{-1, -1};
         }
-        int first = binarysearchfirst(nums, target);
-        if (first == nums.length || nums[first] != target) {
+        int left = binarysearch(nums, target);
+        if (left == nums.length || nums[left] != target) {
             return new int[]{-1, -1};
         }
-        int last = binarysearchfirst(nums, target + 1) - 1;
-        return new int[]{first, last};
+        int right = binarysearch(nums, target + 1) - 1;
+        return new int[]{left, right};
     }
 
-    public int binarysearchfirst(int[] nums, int target) {
+    private int binarysearch(int[] nums, int target) {
         int l = 0;
         int h = nums.length;
         while (l < h) {
             int mid = l + (h - l) / 2;
-            if (nums[mid] == target) {
-                h = mid;
-            } else if (nums[mid] < target) {
+            if (nums[mid] < target) {
                 l = mid + 1;
             } else {
                 h = mid;
