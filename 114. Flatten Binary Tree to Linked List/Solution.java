783b0c311cf795a37f7016195d381284db033bcc
Zhiyuan Ma <631253076@qq.com>
2021-05-14 21:57:31 +0800

    leetcode 114

---
+++ 114. Flatten Binary Tree to Linked List/Solution.java
@@ -0,0 +1,33 @@
+class Solution {
+    private TreeNode prev = null;
+
+    public void flatten(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        flatten(root.right);
+        flatten(root.left);
+        root.right = prev;
+        root.left = null;
+        prev = root;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
