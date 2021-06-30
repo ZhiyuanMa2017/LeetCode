0aadcf23c98b9c6b04b246df8c62a3da112a37b8
Zhiyuan Ma <631253076@qq.com>
2021-07-01 03:55:27 +0800

    leetcode 236

---
+++ 236. Lowest Common Ancestor of a Binary Tree/Solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left == null && right == null) {
+            return null;
+        } else if (left != null && right != null) {
+            return root;
+        } else {
+            return left == null ? right : left;
+        }
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
