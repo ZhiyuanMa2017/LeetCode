b34b1931cad09f5fb3cc89dbd8485844ace21d65
Zhiyuan Ma <631253076@qq.com>
2021-08-20 14:45:06 -0400

    leetcode 101

---
+++ 101. Symmetric Tree/Solution.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        return helper(root.left, root.right);
+    }
+
+    private boolean helper(TreeNode l, TreeNode r) {
+        if (l == null && r == null) {
+            return true;
+        }
+        if (l == null || r == null) {
+            return false;
+        }
+        if (l.val != r.val) {
+            return false;
+        }
+        return helper(l.left, r.right) && helper(l.right, r.left);
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
