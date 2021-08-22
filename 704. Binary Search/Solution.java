9502ce244da57b2a1257c33c9c23246fccc7a080
Zhiyuan Ma <631253076@qq.com>
2021-08-21 22:18:44 -0400

    leetcode 704

---
+++ 704. Binary Search/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int l = 0;
+        int h = nums.length - 1;
+        while (l <= h) {
+            int mid = l + (h - l) / 2;
+            if (target < nums[mid]) {
+                h = mid - 1;
+            } else if (target > nums[mid]) {
+                l = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
