29db7d6d555d8816e6d4d3e03c2a65c9ab05740e
Zhiyuan Ma <631253076@qq.com>
2021-08-24 22:44:13 -0400

    leetcode 876

---
+++ 876. Middle of the Linked List/Solution.java
@@ -0,0 +1,28 @@
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
