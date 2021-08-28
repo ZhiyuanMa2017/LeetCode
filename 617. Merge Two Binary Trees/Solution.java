39539cfe59d6bddd5f3e0544a99aa83bc50fe15a
Zhiyuan Ma <631253076@qq.com>
2021-08-28 01:19:12 -0400

    leetcode 617

---
+++ 617. Merge Two Binary Trees/Solution.java
@@ -0,0 +1,36 @@
+class Solution {
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if (root1 == null && root2 == null) {
+            return null;
+        }
+        if (root1 == null) {
+            return root2;
+        }
+        if (root2 == null) {
+            return root1;
+        }
+        TreeNode thisNode = new TreeNode(root1.val + root2.val);
+        thisNode.left = mergeTrees(root1.left, root2.left);
+        thisNode.right = mergeTrees(root1.right, root2.right);
+        return thisNode;
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
