77fd8aa2aa46d80f8a012e5f1f6e9feea1f11a2c
Zhiyuan Ma <631253076@qq.com>
2021-09-03 17:10:27 -0400

    leetcode 190

---
+++ 190. Reverse Bits/Solution.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public int reverseBits(int n) {
+        int res = 0;
+        for (int i = 0; i < 32; i++) {
+            res <<= 1;
+            res = res | (n & 1);
+            n >>= 1;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Integer.toBinaryString(-1));
+        System.out.println(-1 << 31);
+        System.out.println(Integer.toBinaryString(-1 << 31));
+        System.out.println(-2147483648 << 1);
+        System.out.println(Integer.toBinaryString(-2147483648 << 1));
+        System.out.println(-1 << 32);
+        System.out.println(Integer.toBinaryString(-1 << 32));
+        System.out.println(-1 << 31 << 1);
+        System.out.println(Integer.toBinaryString(-1 << 31 << 1));
+
+    }
+}
