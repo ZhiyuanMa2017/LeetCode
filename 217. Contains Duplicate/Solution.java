b25ec4015d6700ad71639a57586fcd4e6963925b
Zhiyuan Ma <631253076@qq.com>
2021-09-05 02:42:54 -0400

    leetcode 217

---
+++ 217. Contains Duplicate/Solution.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet();
+        for (int num : nums) {
+            if (!set.add(num)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
