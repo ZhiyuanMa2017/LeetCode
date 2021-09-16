989bf56f2ba81b8382b8853997743d8d81987e7a
Zhiyuan Ma <631253076@qq.com>
2021-09-15 23:48:35 -0400

    leetcode 1877

---
+++ 1877. Minimize Maximum Pair Sum in Array/Solution.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+class Solution {
+    public int minPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int res = 0;
+        int n = nums.length;
+        for (int i = 0; i < n / 2; i++) {
+            res = Math.max(res, nums[i] + nums[n - i - 1]);
+        }
+        return res;
+    }
+}
