a481c4f1e0285c8922c3505f197937d4a2a3bdbc
Zhiyuan Ma <631253076@qq.com>
2021-09-15 22:19:40 -0400

    leetcode 11

---
+++ 11. Container With Most Water/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int res = 0;
+        int i = 0;
+        int j = height.length - 1;
+        while (i < j) {
+            res = Math.max(res, Math.min(height[i], height[j]) * (j - i));
+            if (height[i] < height[j]) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return res;
+    }
+}
