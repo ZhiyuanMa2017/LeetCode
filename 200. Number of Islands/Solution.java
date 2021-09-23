d750873a90b8771f8117e90bbfb9f1d5ae618ab0
Zhiyuan Ma <631253076@qq.com>
2021-09-22 23:39:57 -0400

    leetcode 200

---
+++ 200. Number of Islands/Solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        int res = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    res++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return res;
+    }
+
+    private void dfs(char[][] grid, int i, int j) {
+        if (i >= 0 && i < grid.length && j >= 0 && j < grid[0].length && grid[i][j] == '1') {
+            grid[i][j] = '2';
+            dfs(grid, i - 1, j);
+            dfs(grid, i + 1, j);
+            dfs(grid, i, j - 1);
+            dfs(grid, i, j + 1);
+        }
+    }
+}
