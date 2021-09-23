a13505ecea39b7a0bfc0d62e9e733540dda1a170
Zhiyuan Ma <631253076@qq.com>
2021-09-23 15:12:13 -0400

    leetcode 1328

---
+++ 1328. Break a Palindrome/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String breakPalindrome(String palindrome) {
+        if (palindrome.length() == 1) {
+            return "";
+        }
+        char[] s = palindrome.toCharArray();
+        for (int i = 0; i < s.length / 2; i++) {
+            if (s[i] != 'a') {
+                s[i] = 'a';
+                return String.valueOf(s);
+            }
+        }
+        s[s.length - 1] = 'b';
+        return String.valueOf(s);
+    }
+}
