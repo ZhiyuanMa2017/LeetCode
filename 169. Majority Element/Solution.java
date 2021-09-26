571d6bda95c538921d624b6a4e882748cfa7670c
Zhiyuan Ma <631253076@qq.com>
2021-09-26 15:32:47 -0400

    leetcode 169

---
+++ 169. Majority Element/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int majority = nums[0];
+        int count = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (count == 0) {
+                count++;
+                majority = nums[i];
+            } else if (majority == nums[i]) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        return majority;
+    }
+}
