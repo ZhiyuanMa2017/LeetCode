e6222565fbe495c4249c7212d74953767de632d9
Zhiyuan Ma <631253076@qq.com>
2021-09-27 20:17:21 -0400

    leetcode 237

---
+++ 237. Delete Node in a Linked List/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
