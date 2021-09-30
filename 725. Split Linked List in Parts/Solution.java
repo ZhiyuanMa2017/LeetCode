927976afcead81a57d92d67f514a812a8cfd6271
Zhiyuan Ma <631253076@qq.com>
2021-09-29 20:50:47 -0400

    leetcode 725

--- 725. Split Linked List in Parts/Solution.java
+++ 725. Split Linked List in Parts/Solution.java
@@ -1,66 +1,49 @@
-import com.sun.source.tree.BreakTree;
-
-import java.sql.PreparedStatement;
-import java.util.List;
-
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- * int val;
- * ListNode next;
- * ListNode() {}
- * ListNode(int val) { this.val = val; }
- * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode[] splitListToParts(ListNode root, int k) {
-        ListNode[] result = new ListNode[k];
-        int len = 0;
+        ListNode[] res = new ListNode[k];
+        int count = 0;
         ListNode head = root;
         while (head != null) {
-            len++;
+            count++;
             head = head.next;
         }
-        int lenthofeach = len / k;
-        int mod = len % k;
-        ListNode curr = root;
+        int len = count / k;
+        int remainder = count % k;
+        ListNode cur = root;
         for (int i = 0; i < k; i++) {
-            if (curr == null) {
+            if (cur == null) {
                 break;
             }
-            result[i] = curr;
-            int thislenth = 0;
-            if (mod > 0) {
-                thislenth = lenthofeach + 1;
-                mod--;
-            } else {
-                thislenth = lenthofeach;
+            res[i] = cur;
+            int thisLen = len;
+            if (remainder > 0) {
+                thisLen++;
+                remainder--;
             }
-            for (int i1 = 0; i1 < thislenth - 1; i1++) {
-                curr = curr.next;
+            for (int j = 0; j < thisLen - 1; j++) {
+                cur = cur.next;
             }
-            ListNode tmp = curr.next;
-            curr.next = null;
-            curr = tmp;
+            ListNode nextNode = cur.next;
+            cur.next = null;
+            cur = nextNode;
         }
-        return result;
-    }
-}
-
-class ListNode {
-    int val;
-    ListNode next;
-
-    ListNode() {
+        return res;
     }
 
-    ListNode(int val) {
-        this.val = val;
-    }
+    class ListNode {
+        int val;
+        ListNode next;
 
-    ListNode(int val, ListNode next) {
-        this.val = val;
-        this.next = next;
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
     }
 }
