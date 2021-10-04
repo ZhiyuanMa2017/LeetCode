30622776a422ac4de8177ec489770c89363fe542
Zhiyuan Ma <631253076@qq.com>
2021-10-03 23:46:23 -0400

    leetcode 463

---
+++ 463. Island Perimeter/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+
+    public int islandPerimeter(int[][] grid) {
+        int res = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 1) {
+                    res += 4;
+                    if (i > 0 && grid[i - 1][j] == 1) {
+                        res -= 2;
+                    }
+                    if (j > 0 && grid[i][j - 1] == 1) {
+                        res -= 2;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
