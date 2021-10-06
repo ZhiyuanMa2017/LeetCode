9428eea4fb44369a2b0204642f0917809982d555
Zhiyuan Ma <631253076@qq.com>
2021-10-05 22:20:29 -0400

    leetcode 442

---
+++ 442. Find All Duplicates in an Array/Solution.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            int index = Math.abs(nums[i]) - 1;
+            //i -> nums[i-1] negative
+            if (nums[index] < 0) {
+                res.add(index + 1);
+            }
+            nums[index] = -nums[index];
+        }
+        return res;
+    }
+}
