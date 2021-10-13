6bbd71524da8b0842d0c758ca68c1d433770a14c
Zhiyuan Ma <631253076@qq.com>
2021-10-13 03:22:45 -0400

    leetcode 547

---
+++ 547. Number of Provinces/Solution.java
@@ -0,0 +1,55 @@
+class Solution {
+
+    class UF {
+        int[] parent;
+        int[] size;
+        int count;
+
+        UF(int n) {
+            count = n;
+            parent = new int[n];
+            size = new int[n];
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+                size[i] = 1;
+            }
+        }
+
+        public int find(int i) {
+            while (i != parent[i]) {
+                parent[i] = parent[parent[i]];
+                i = parent[i];
+            }
+            return i;
+        }
+
+        public void union(int i, int j) {
+            int p = find(i);
+            int q = find(j);
+            if (q == p) {
+                return;
+            }
+            if (size[p] >= size[q]) {
+                parent[q] = p;
+                size[p] += size[q];
+            } else {
+                parent[p] = q;
+                size[q] += size[p];
+            }
+            count--;
+        }
+    }
+
+    public int findCircleNum(int[][] isConnected) {
+        int n = isConnected.length;
+        UF uf = new UF(n);
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (isConnected[i][j] == 1) {
+                    uf.union(i, j);
+                }
+            }
+        }
+        return uf.count;
+    }
+}
