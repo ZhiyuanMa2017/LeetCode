b3764b344a50ad5f9c3de1ebb91759650042f644
Zhiyuan Ma <631253076@qq.com>
2021-10-15 18:58:34 -0400

    leetcode 188

---
+++ 188. Best Time to Buy and Sell Stock IV/Solution.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+class Solution {
+    public int maxProfit(int k, int[] prices) {
+        if (k >= prices.length / 2) {
+            int profit = 0;
+            for (int i = 0; i < prices.length - 1; i++) {
+                if (prices[i + 1] > prices[i]) {
+                    profit += prices[i + 1] - prices[i];
+                }
+            }
+            return profit;
+        }
+        int[] buy = new int[k + 1];
+        int[] sell = new int[k + 1];
+        Arrays.fill(buy, Integer.MIN_VALUE);
+        for (int i = 0; i < prices.length; i++) {
+            for (int j = 1; j <= k; j++) {
+                buy[j] = Math.max(buy[j], sell[j - 1] - prices[i]);
+                sell[j] = Math.max(sell[j], buy[j] + prices[i]);
+            }
+        }
+        return sell[k];
+    }
+}
