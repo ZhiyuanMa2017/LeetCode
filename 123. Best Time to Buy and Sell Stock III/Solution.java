a97b78d47a67df137f8b8b15372de42f6aec1157
Zhiyuan Ma <631253076@qq.com>
2021-10-15 18:50:38 -0400

    leetcode leetcode 123

--- 123. Best Time to Buy and Sell Stock III/Solution.java
+++ 123. Best Time to Buy and Sell Stock III/Solution.java
@@ -1,15 +1,15 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int buyOne = -prices[0];
-        int sellOne = Integer.MIN_VALUE;
+        int sellOne = 0;
         int buyTwo = Integer.MIN_VALUE;
-        int sellTwo = Integer.MIN_VALUE;
+        int sellTwo = 0;
         for (int i = 1; i < prices.length; i++) {
             buyOne = Math.max(buyOne, -prices[i]);
             sellOne = Math.max(sellOne, buyOne + prices[i]);
             buyTwo = Math.max(buyTwo, sellOne - prices[i]);
             sellTwo = Math.max(sellTwo, buyTwo + prices[i]);
         }
-        return Math.max(sellTwo, 0);
+        return sellTwo;
     }
 }
