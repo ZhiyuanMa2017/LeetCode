6392511e4351e72a273e5b994384c8d523781e8b
Zhiyuan Ma <631253076@qq.com>
2021-10-19 00:15:47 -0400

    leetcode 503

---
+++ 503. Next Greater Element II/Solution.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.Stack;
+
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        Arrays.fill(res, -1);
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < n * 2; i++) {
+            while (!stack.isEmpty() && nums[stack.peek()] < nums[i % n]) {
+                res[stack.pop()] = nums[i % n];
+            }
+            stack.push(i % n);
+        }
+        return res;
+    }
+}
