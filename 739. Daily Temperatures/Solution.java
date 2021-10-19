7c1b5ecd5deeac5f8399eaeb45a6c337f8955077
Zhiyuan Ma <631253076@qq.com>
2021-10-19 01:39:24 -0400

    leetcode 739

---
+++ 739. Daily Temperatures/Solution.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.Stack;
+
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] res = new int[temperatures.length];
+        Stack<Integer> stack = new Stack<>();
+        Arrays.fill(res, 0);
+        for (int i = 0; i < temperatures.length; i++) {
+            while (!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]) {
+                res[stack.peek()] = i - stack.pop();
+            }
+            stack.push(i);
+        }
+        return res;
+    }
+}
