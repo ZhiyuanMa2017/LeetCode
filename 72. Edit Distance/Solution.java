ac3e393bacaefcabd0508f8196dc6e8c970d24ae
Zhiyuan Ma <631253076@qq.com>
2021-10-28 20:05:45 -0400

    leetcode 72

---
+++ 72. Edit Distance/Solution.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int n = word1.length();
+        int m = word2.length();
+        if (n * m == 0) {
+            return n + m;
+        }
+        int[][] dp = new int[n + 1][m + 1];
+        char[] from = (" " + word1).toCharArray();
+        char[] to = (" " + word2).toCharArray();
+        for (int i = 1; i <= n; i++) {
+            dp[i][0] = i;
+        }
+        for (int j = 1; j <= m; j++) {
+            dp[0][j] = j;
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (from[i] == to[j]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    int insert = dp[i][j - 1] + 1;
+                    int delete = dp[i - 1][j] + 1;
+                    int replace = dp[i - 1][j - 1] + 1;
+                    dp[i][j] = Math.min(insert, Math.min(delete, replace));
+                }
+            }
+        }
+        return dp[n][m];
+    }
+}
