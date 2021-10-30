14926fa00682c4f77b29fae9b7685675cc34fbb3
Zhiyuan Ma <631253076@qq.com>
2021-10-30 19:11:28 -0400

    leetcode 96

---
+++ 96. Unique Binary Search Trees/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numTrees(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                dp[i] += dp[j - 1] * dp[i - j];
+            }
+        }
+        //Just treat each number as root, and then left part * right part is the answer.
+        return dp[n];
+    }
+}
