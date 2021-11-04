9f6f05e1bf37b72a0f303e2c88a77fbab3b3e04e
Zhiyuan Ma <631253076@qq.com>
2021-11-04 02:33:45 -0400

    leetcode 113

---
+++ 113. Path Sum II/Solution.java
@@ -0,0 +1,52 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+class Solution {
+    private List<List<Integer>> res;
+
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        backtrack(root, new ArrayList<>(), targetSum);
+        return res;
+    }
+
+    private void backtrack(TreeNode root, List<Integer> path, int target) {
+        path.add(root.val);
+        if (root.left == null && root.right == null) {
+            if (target == root.val) {
+                res.add(new ArrayList<>(path));
+            }
+        }
+        if (root.left != null) {
+            backtrack(root.left, path, target - root.val);
+        }
+        if (root.right != null) {
+            backtrack(root.right, path, target - root.val);
+        }
+        path.remove(path.size() - 1);
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
