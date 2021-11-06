4548c5af85d6c02daf8a0488ea817b2d3b40fe6f
Zhiyuan Ma <631253076@qq.com>
2021-11-06 01:18:04 -0400

    leetcode 139

---
+++ 139. Word Break/Solution.java
@@ -0,0 +1,20 @@
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        boolean[] dp = new boolean[s.length() + 1];
+        Set<String> set = new HashSet<>(wordDict);
+        dp[0] = true;
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && set.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+}
