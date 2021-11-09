defe6ea6d0f7bfc3f9b6b6f78bbe1c7a67d5b87d
Zhiyuan Ma <631253076@qq.com>
2021-11-09 01:51:52 -0500

    leetcode 168

--- 168. Excel Sheet Column Title/Solution.java
+++ 168. Excel Sheet Column Title/Solution.java
@@ -3,7 +3,7 @@ class Solution {
         StringBuilder sb = new StringBuilder();
         while (columnNumber > 0) {
             columnNumber--;
-            sb.append((char) 'A' + columnNumber % 26);
+            sb.append((char) ('A' + columnNumber % 26));
             columnNumber /= 26;
         }
         return sb.reverse().toString();
