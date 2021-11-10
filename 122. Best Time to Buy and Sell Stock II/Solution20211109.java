f1f24ae7e3a2720a5b2530cea7732b7fbec03aa9
Zhiyuan Ma <631253076@qq.com>
2021-11-09 20:40:15 -0500

    leetcode 122

---
+++ 122. Best Time to Buy and Sell Stock II/Solution20211109.java
@@ -0,0 +1,11 @@
+class Solution20211109 {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        for (int i = 0; i < prices.length - 1; i++) {
+            if (prices[i + 1] > prices[i]) {
+                profit += prices[i + 1] - prices[i];
+            }
+        }
+        return profit;
+    }
+}
