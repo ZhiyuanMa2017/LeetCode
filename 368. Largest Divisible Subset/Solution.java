02e68ff6a1f61ae781ebd38a2b20005a29beb88f
Zhiyuan Ma <631253076@qq.com>
2021-11-14 23:38:19 -0500

    leetcode 368

---
+++ 368. Largest Divisible Subset/Solution.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    public List<Integer> largestDivisibleSubset(int[] nums) {
+        int[] dp = new int[nums.length];
+        int[] prev = new int[nums.length];
+        Arrays.sort(nums);
+        int max = 0;
+        int index = -1;
+        for (int i = 0; i < nums.length; i++) {
+            dp[i] = 1;
+            prev[i] = -1;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] % nums[j] == 0) {
+                    if (dp[i] < dp[j] + 1) {
+                        dp[i] = dp[j] + 1;
+                        prev[i] = j;
+                    }
+                }
+            }
+            if (dp[i] > max) {
+                max = dp[i];
+                index = i;
+            }
+        }
+        List<Integer> res = new ArrayList<>();
+        while (index != -1) {
+            res.add(nums[index]);
+            index = prev[index];
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
