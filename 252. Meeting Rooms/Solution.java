ff47f9fd3c4bee24fd8d533438b09ce566167af7
Zhiyuan Ma <631253076@qq.com>
2021-11-27 17:53:08 -0500

    leetocde 252

---
+++ 252. Meeting Rooms/Solution.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+class Solution {
+    public boolean canAttendMeetings(int[][] intervals) {
+        Arrays.sort(intervals, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] - o2[0];
+            }
+        });
+        for (int i = 0; i < intervals.length - 1; i++) {
+            if (intervals[i][1] > intervals[i + 1][0]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
