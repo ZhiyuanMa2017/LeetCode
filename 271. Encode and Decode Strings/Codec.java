9a244911162b1c1127563f6b80228ea67bc0974c
Zhiyuan Ma <631253076@qq.com>
2021-12-11 16:37:12 -0500

    leetcode 271

---
+++ 271. Encode and Decode Strings/Codec.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Codec {
+
+    // Encodes a list of strings to a single string.
+    public String encode(List<String> strs) {
+        StringBuilder sb = new StringBuilder();
+        for (String str : strs) {
+            sb.append(str.length()).append(":").append(str);
+        }
+        return sb.toString();
+    }
+
+    // Decodes a single string to a list of strings.
+    public List<String> decode(String s) {
+        List<String> res = new ArrayList<>();
+        int index = 0;
+        while (index < s.length()) {
+            int colon = s.indexOf(":", index);
+            int size = Integer.parseInt(s.substring(index, colon));
+            index = colon + size + 1;
+            res.add(s.substring(colon + 1, index));
+        }
+        return res;
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.decode(codec.encode(strs));
