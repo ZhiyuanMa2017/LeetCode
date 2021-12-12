a3b7695633f4472753c6ba3cda20b35a440c396a
Zhiyuan Ma <631253076@qq.com>
2021-12-12 02:22:14 -0500

    leetcode 2104

---
+++ 2104. Sum of Subarray Ranges/Solution.java
@@ -0,0 +1,34 @@
+import java.util.Stack;
+
+class Solution2 {
+    public long subArrayRanges(int[] A) {
+        int n = A.length, j, k;
+        long res = 0;
+
+        Stack<Integer> s = new Stack<>();
+        for (int i = 0; i <= n; i++) {
+            while (!s.isEmpty() && A[s.peek()] > (i == n ? Integer.MIN_VALUE : A[i])) {
+                j = s.pop();
+                k = s.isEmpty() ? -1 : s.peek();
+                res -= (long) A[j] * (i - j) * (j - k);
+            }
+            s.push(i);
+        }
+        s.clear();
+        for (int i = 0; i <= n; i++) {
+            while (!s.isEmpty() && A[s.peek()] < (i == n ? Integer.MAX_VALUE : A[i])) {
+                j = s.pop();
+                k = s.isEmpty() ? -1 : s.peek();
+                res += (long) A[j] * (i - j) * (j - k);
+
+            }
+            s.push(i);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Solution2 s = new Solution2();
+        s.subArrayRanges(new int[]{3, 1, 2, 4});
+    }
+}
