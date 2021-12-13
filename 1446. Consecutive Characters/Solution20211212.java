fc8c5d02c356d3850f9926de06e9a11d3ba03f6a
Zhiyuan Ma <631253076@qq.com>
2021-12-12 20:33:53 -0500

    leetcode 1446

--- 1446. Consecutive Characters/Solution20211212.java
+++ 1446. Consecutive Characters/Solution20211212.java
@@ -1,15 +1,15 @@
-class Solution {
+class Solution20211212 {
     public int maxPower(String s) {
-        int cnt = 1;
         int res = 1;
+        int count = 1;
         for (int i = 1; i < s.length(); i++) {
             if (s.charAt(i) == s.charAt(i - 1)) {
-                cnt += 1;
-                res = Math.max(res, cnt);
+                count++;
             } else {
-                cnt = 1;
+                res = Math.max(res, count);
+                count = 1;
             }
         }
-        return res;
+        return Math.max(res, count);
     }
 }
