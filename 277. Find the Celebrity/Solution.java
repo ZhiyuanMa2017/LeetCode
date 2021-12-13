ecef669681600184990005473471de209f78a8a6
Zhiyuan Ma <631253076@qq.com>
2021-12-13 17:01:21 -0500

    leetcode 277

---
+++ 277. Find the Celebrity/Solution.java
@@ -0,0 +1,22 @@
+/* The knows API is defined in the parent class Relation.
+      boolean knows(int a, int b); */
+
+public class Solution extends Relation {
+    public int findCelebrity(int n) {
+        int celebrity = 0;
+        for (int i = 1; i < n; i++) {
+            if (knows(celebrity, i)) {
+                celebrity = i;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (i == celebrity) {
+                continue;
+            }
+            if (!knows(i, celebrity) || knows(celebrity, i)) {
+                return -1;
+            }
+        }
+        return celebrity;
+    }
+}
