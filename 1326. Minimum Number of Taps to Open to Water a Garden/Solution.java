863182948c720c7a16bb1ac51d29c20c835b64f0
Zhiyuan Ma <631253076@qq.com>
2021-12-19 16:14:14 -0500

    leetcode 1326

--- 1326. Minimum Number of Taps to Open to Water a Garden/Solution.java
+++ 1326. Minimum Number of Taps to Open to Water a Garden/Solution.java
@@ -5,24 +5,22 @@ class Solution {
             int left = Math.max(0, i - ranges[i]);
             jumpGamer[left] = Math.max(jumpGamer[left], i + ranges[i]);
         }
-        int res = 0, curEnd = 0, curFarthest = 0;
+        int res = 0, curEnd = 0, curFarest = 0;
         int index = 0;
         while (index <= curEnd) {
             while (index <= curEnd) {
-                curFarthest = Math.max(curFarthest, jumpGamer[index]);
+                curFarest = Math.max(curFarest, jumpGamer[index]);
                 index++;
-                if (curFarthest >= n) {
+                if (curFarest >= n) {
                     return res + 1;
                 }
             }
-            curEnd = curFarthest;
+            if (curEnd == curFarest) {
+                return -1;
+            }
+            curEnd = curFarest;
             res++;
         }
         return -1;
-    }
-
-    public static void main(String[] args) {
-        Solution s = new Solution();
-        s.minTaps(5, new int[]{0, 0, 0, 0, 0, 0});
     }
 }
