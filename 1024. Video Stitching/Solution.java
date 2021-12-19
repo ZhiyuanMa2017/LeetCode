05d904a50a0f9d50a741f6cc99c96d203e5941b2
Zhiyuan Ma <631253076@qq.com>
2021-12-19 16:04:59 -0500

    leetcode 1024

---
+++ 1024. Video Stitching/Solution.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int videoStitching(int[][] clips, int time) {
+        int[] farthestCanReach = new int[time];
+        for (int[] clip : clips) {
+            int start = clip[0];
+            if (start < time) {
+                farthestCanReach[start] = Math.max(farthestCanReach[start], clip[1]);
+            }
+
+        }
+        int index = 0;
+        int curEnd = 0;
+        int curFarthest = 0;
+        int res = 0;
+        while (index <= curEnd) {
+            while (index <= curEnd) {
+                curFarthest = Math.max(curFarthest, farthestCanReach[index]);
+                index++;
+                if (curFarthest >= time) {
+                    return res + 1;
+                }
+            }
+            res++;
+            curEnd = curFarthest;
+        }
+        return -1;
+    }
+}
