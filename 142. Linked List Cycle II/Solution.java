a685503e3f3ace179ed7c7b744a50bf5fe47ce5a
Zhiyuan Ma <631253076@qq.com>
2022-01-18 21:55:09 -0500

    leetcode 142

--- 142. Linked List Cycle II/Solution.java
+++ 142. Linked List Cycle II/Solution.java
@@ -1,13 +1,3 @@
-class ListNode {
-    int val;
-    ListNode next;
-
-    ListNode(int x) {
-        val = x;
-        next = null;
-    }
-}
-
 public class Solution {
     public ListNode detectCycle(ListNode head) {
         ListNode slow = head;
