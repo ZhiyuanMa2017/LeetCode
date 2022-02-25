e3083fc4a92f5d8c917a4c037fd89e0babb54758
Zhiyuan Ma <631253076@qq.com>
2022-02-25 17:06:23 -0500

    leetcode 1064

---
+++ 1064. Fixed Point/Solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int fixedPoint(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (i == arr[i]) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
