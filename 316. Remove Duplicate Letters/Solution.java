1c419f96285f220b80bedf674fd8ce59d321f6eb
Zhiyuan Ma <631253076@qq.com>
2022-03-17 20:40:54 -0400

    leetcode 316

---
+++ 316. Remove Duplicate Letters/Solution.java
@@ -0,0 +1,37 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+class Solution {
+    public String removeDuplicateLetters(String s) {
+        int[] count = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+        }
+        boolean[] used = new boolean[26];
+        Deque<Character> stack = new ArrayDeque<>();
+        for (int i = 0; i < s.length(); i++) {
+
+            char c = s.charAt(i);
+            count[c - 'a']--;
+            if (used[c - 'a']) {
+                continue;
+            }
+            if (stack.isEmpty()) {
+                stack.push(c);
+                used[c - 'a'] = true;
+            } else {
+                while (!stack.isEmpty() && stack.peek() > c && count[stack.peek() - 'a'] > 0) {
+                    char pop = stack.pop();
+                    used[pop - 'a'] = false;
+                }
+                stack.push(c);
+                used[c - 'a'] = true;
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+        return sb.reverse().toString();
+    }
+}
