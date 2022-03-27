b2f96da965d04373a7d87244c704c620d28475b0
Zhiyuan Ma <631253076@qq.com>
2022-03-27 18:54:42 -0400

    leetcode 455

---
+++ 455. Assign Cookies/Solution.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int i = 0;
+        int j = 0;
+        while (i < g.length && j < s.length) {
+            if (g[i] <= s[j]) {
+                i++;
+                j++;
+            } else {
+                j++;
+            }
+        }
+        return i;
+    }
+}
