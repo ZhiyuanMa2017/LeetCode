58736c8d4d1c7689457e556766c9633b78080578
Zhiyuan Ma <631253076@qq.com>
2022-05-30 19:29:51 -0400

    leetcode 2053

---
+++ 2053. Kth Distinct String in an Array/Solution.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public String kthDistinct(String[] arr, int k) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String s : arr) {
+            map.put(s, map.getOrDefault(s, 0) + 1);
+        }
+        for (String s : arr) {
+            if (map.get(s) == 1) {
+                k--;
+                if (k == 0) {
+                    return s;
+                }
+            }
+        }
+        return "";
+    }
+}
