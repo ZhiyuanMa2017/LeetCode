3b2a3aaa3191966f7d348a0718e741e2c594805c
Zhiyuan Ma <631253076@qq.com>
2022-05-30 20:47:55 -0400

    leetcode 1136

---
+++ 1136. Parallel Courses/Solution.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+class Solution {
+    public int minimumSemesters(int n, int[][] relations) {
+        List<List<Integer>> graph = new ArrayList<>();
+        int[] inDegree = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
+        }
+        for (int[] relation : relations) {
+            int prev = relation[0];
+            int next = relation[1];
+            inDegree[next]++;
+            graph.get(prev).add(next);
+        }
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i < inDegree.length; i++) {
+            if (inDegree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+        int step = 0;
+        int coursesTaken = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            coursesTaken += size;
+            for (int i = 0; i < size; i++) {
+                int cur = queue.poll();
+                for (Integer next : graph.get(cur)) {
+                    inDegree[next]--;
+                    if (inDegree[next] == 0) {
+                        queue.offer(next);
+                    }
+                }
+            }
+            step++;
+        }
+        if (coursesTaken == n) {
+            return step;
+        }
+        return -1;
+    }
+}
