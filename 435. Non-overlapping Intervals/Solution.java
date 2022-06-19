0faee2aee4162abbab35997830f6a34dcc07eb72
Zhiyuan Ma <631253076@qq.com>
2022-06-18 21:10:25 -0400

    leetcode 435

---
+++ 435. Non-overlapping Intervals/Solution.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+class Solution {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> a[1] - b[1]);
+        int end = intervals[0][1];
+        int count = 1;
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] >= end) {
+                count++;
+                end = intervals[i][1];
+            }
+        }
+        return intervals.length - count;
+    }
+}
