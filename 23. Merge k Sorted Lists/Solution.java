f1dfe4394736b5d91bfe59427b9ab4f4f84c8fa9
Zhiyuan Ma <631253076@qq.com>
2022-06-19 23:50:53 -0400

    leetcode 23

--- 23. Merge k Sorted Lists/Solution.java
+++ 23. Merge k Sorted Lists/Solution.java
@@ -1,47 +1,23 @@
-import java.util.Comparator;
 import java.util.PriorityQueue;
 
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        PriorityQueue<ListNode> pq = new PriorityQueue<>(new Comparator<ListNode>() {
-            @Override
-            public int compare(ListNode o1, ListNode o2) {
-                return o1.val - o2.val;
-            }
-        });
-        for (int i = 0; i < lists.length; i++) {
-            if(lists[i]!=null){
-                pq.add(lists[i]);
+        PriorityQueue<ListNode> pq = new PriorityQueue<>((a, b) -> a.val - b.val);
+        for (ListNode list : lists) {
+            if (list != null) {
+                pq.add(list);
             }
         }
-        ListNode sentinel = new ListNode(0);
+        ListNode sentinel = new ListNode(-1);
         ListNode cur = sentinel;
         while (!pq.isEmpty()) {
-            ListNode node = pq.poll();
-            cur.next = node;
+            ListNode listNode = pq.poll();
+            cur.next = listNode;
             cur = cur.next;
-            if (node.next!=null) {
-                pq.add(node.next);
+            if (listNode.next != null) {
+                pq.offer(listNode.next);
             }
         }
-        cur.next = null;
         return sentinel.next;
-    }
-}
-
-class ListNode {
-    int val;
-    ListNode next;
-
-    ListNode() {
-    }
-
-    ListNode(int val) {
-        this.val = val;
-    }
-
-    ListNode(int val, ListNode next) {
-        this.val = val;
-        this.next = next;
     }
 }
