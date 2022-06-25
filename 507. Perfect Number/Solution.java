1d8f8748c29d86639e3e9c35e4750057dccaeea8
Zhiyuan Ma <631253076@qq.com>
2022-06-25 15:39:47 -0400

    leetcode 507

---
+++ 507. Perfect Number/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        if (num == 1) {
+            return false;
+        }
+        int sum = 1;
+        for (int i = 2; i <= num / i; i++) {
+            if (num % i == 0) {
+                sum += i;
+                if (i * i != num) {
+                    sum += num / i;
+                }
+            }
+        }
+        return sum == num;
+    }
+}
