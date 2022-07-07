ec3bf3ebc70458c52985ebff963dfd3e22ef329b
Zhiyuan Ma <631253076@qq.com>
2022-07-06 20:20:39 -0400

    leetcode 97

---
+++ 97. Interleaving String/Solution2.java
@@ -0,0 +1,36 @@
+class Solution2 {
+    public boolean isInterleave(String s1, String s2, String s3) {
+        int m = s1.length();
+        int n = s2.length();
+        if (m + n != s3.length()) {
+            return false;
+        }
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
+        for (int i = 1; i <= m; i++) {
+            if (s1.charAt(i - 1) == s3.charAt(i - 1)) {
+                dp[i][0] = true;
+            } else {
+                break;
+            }
+        }
+        for (int j = 1; j <= n; j++) {
+            if (s2.charAt(j - 1) == s3.charAt(j - 1)) {
+                dp[0][j] = true;
+            } else {
+                break;
+            }
+        }
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (dp[i - 1][j] && s1.charAt(i - 1) == s3.charAt(i + j - 1)) {
+                    dp[i][j] = true;
+                }
+                if (dp[i][j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1)) {
+                    dp[i][j] = true;
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
