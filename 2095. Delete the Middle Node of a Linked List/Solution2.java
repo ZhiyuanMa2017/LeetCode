9e0467b1c1483ed49d576e106af816d4aeb9938d
Zhiyuan Ma <631253076@qq.com>
2022-10-13 20:22:40 -0400

    leetcode 2095

---
+++ 2095. Delete the Middle Node of a Linked List/Solution2.java
@@ -0,0 +1,15 @@
+class Solution {
+    public ListNode deleteMiddle(ListNode head) {
+        if (head.next == null) {
+            return null;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        slow.next = slow.next.next;
+        return head;
+    }
+}
