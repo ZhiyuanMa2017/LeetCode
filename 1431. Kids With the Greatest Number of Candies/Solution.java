fa6cc51995551c375068b8090a20413bfd535793
Zhiyuan Ma <631253076@qq.com>
2022-10-29 20:27:02 -0400

    leetcode 1431

---
+++ 1431. Kids With the Greatest Number of Candies/Solution.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int max = 0;
+        for (int candy : candies) {
+            max = Math.max(max, candy);
+        }
+        List<Boolean> res = new ArrayList<>();
+        for (int candy : candies) {
+            if (candy + extraCandies >= max) {
+                res.add(true);
+            } else {
+                res.add(false);
+            }
+        }
+        return res;
+    }
+}
