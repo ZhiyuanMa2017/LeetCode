f735e1f2174522b160615092134d99a2b20909a1
Zhiyuan Ma <631253076@qq.com>
2022-10-30 01:38:35 -0400

    leetcode 2455

---
+++ 2455. Average Value of Even Numbers That Are Divisible by Three/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int averageValue(int[] nums) {
+        int sum = 0;
+        int count = 0;
+        for (int num : nums) {
+            if (num % 6 == 0) {
+                sum += num;
+                count++;
+            }
+        }
+        if (count == 0) {
+            return 0;
+        }
+        return sum / count;
+    }
+}
