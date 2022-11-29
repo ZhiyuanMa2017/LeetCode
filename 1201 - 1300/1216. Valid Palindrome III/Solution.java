14ec876a56823cc2b5d5c9b4e571b6f2abfced85
Zhiyuan Ma <631253076@qq.com>
2022-11-28 20:53:53 -0500

    leetcode 1216

---
+++ 1201 - 1300/1216. Valid Palindrome III/Solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isValidPalindrome(String s, int k) {
+        int n = s.length();
+        int[][] dp = new int[n][n];
+        for (int len = 0; len < n; len++) {
+            for (int l = 0; l + len < n; l++) {
+                int r = l + len;
+                if (len == 0) {
+                    dp[l][r] = 1;
+                } else if (len == 1) {
+                    if (s.charAt(l) == s.charAt(r)) {
+                        dp[l][r] = 2;
+                    } else {
+                        dp[l][r] = 1;
+                    }
+                } else {
+                    dp[l][r] = Math.max(dp[l + 1][r], dp[l][r - 1]);
+                    if (s.charAt(l) == s.charAt(r)) {
+                        dp[l][r] = Math.max(dp[l][r], dp[l + 1][r - 1] + 2);
+                    }
+                }
+            }
+        }
+        return dp[0][n - 1] + k >= s.length();
+    }
+}
