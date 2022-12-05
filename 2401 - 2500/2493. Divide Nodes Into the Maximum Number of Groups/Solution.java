7656af43f32990523e9a82d74d62d561f57ead0c
Zhiyuan Ma <631253076@qq.com>
2022-12-04 21:55:57 -0500

    leetcode 2493

--- 2401 - 2500/2493. Divide Nodes Into the Maximum Number of Groups/Solution.java
+++ 2401 - 2500/2493. Divide Nodes Into the Maximum Number of Groups/Solution.java
@@ -12,27 +12,17 @@ class Solution {
             int b = edge[1];
             graph[a][b] = true;
             graph[b][a] = true;
-            uf.degree[a]++;
-            uf.degree[b]++;
-        }
-        for (int[] edge : edges) {
-            int a = edge[0];
-            int b = edge[1];
             uf.union(a, b);
         }
         int res = 0;
         Map<Integer, Integer> pToV = new HashMap<>();
         for (int i = 1; i <= n; i++) {
             int p = uf.find(i);
-            if (i != p && uf.degree[i] > uf.degree[p]) {
-                continue;
-            }
             int cur = bfs(graph, i);
             if (cur == -1) {
                 return -1;
             }
             pToV.put(p, Math.max(cur, pToV.getOrDefault(p, 0)));
-
         }
         for (Integer value : pToV.values()) {
             res += value;
@@ -46,13 +36,13 @@ class Solution {
         n -= 1;
         int[] level = new int[n + 1];
         level[start] = 1;
-        int res = 1;
+        int res = 0;
         queue.offer(start);
         while (!queue.isEmpty()) {
             int size = queue.size();
+            res++;
             for (int i = 0; i < size; i++) {
                 int cur = queue.poll();
-                res = Math.max(res, level[cur]);
                 for (int j = 1; j <= n; j++) {
                     if (graph[cur][j]) {
                         if (level[j] == 0) {
@@ -74,14 +64,14 @@ class Solution {
 
     class UF {
         int[] parent;
-        int[] degree;
+        int[] size;
 
         UF(int n) {
             parent = new int[n];
-            degree = new int[n];
+            size = new int[n];
             for (int i = 0; i < n; i++) {
                 parent[i] = i;
-                degree[i] = 0;
+                size[i] = 1;
             }
         }
 
@@ -99,10 +89,12 @@ class Solution {
             if (q == p) {
                 return;
             }
-            if (degree[p] <= degree[q]) {
+            if (size[p] >= size[q]) {
                 parent[q] = p;
+                size[p] += size[q];
             } else {
                 parent[p] = q;
+                size[q] += size[p];
             }
         }
     }
