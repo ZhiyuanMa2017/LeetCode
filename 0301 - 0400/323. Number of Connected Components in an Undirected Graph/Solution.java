18560e124b091d91c9b32f8b87437eabebedc39a
Zhiyuan Ma <631253076@qq.com>
2022-12-07 19:23:16 -0500

    leetcode 323

---
+++ 0301 - 0400/323. Number of Connected Components in an Undirected Graph/Solution.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int countComponents(int n, int[][] edges) {
+        UF uf = new UF(n);
+        for (int[] edge : edges) {
+            uf.union(edge[0], edge[1]);
+        }
+        return uf.count;
+    }
+
+    class UF {
+        int[] parent;
+        int[] size;
+        int count;
+
+        UF(int n) {
+            parent = new int[n];
+            size = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+                size[i] = 1;
+            }
+            count = n;
+        }
+
+        public int find(int i) {
+            while (i != parent[i]) {
+                parent[i] = parent[parent[i]];
+                i = parent[i];
+            }
+            return i;
+        }
+
+        public void union(int i, int j) {
+            int p = find(i);
+            int q = find(j);
+            if (p == q) {
+                return;
+            }
+            count--;
+            if (size[p] >= size[q]) {
+                parent[q] = p;
+                size[p] += size[q];
+            } else {
+                parent[p] = q;
+                size[q] += size[p];
+            }
+        }
+    }
+}
