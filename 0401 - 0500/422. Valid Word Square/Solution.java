fac3cee79e7fc413344f80365bb25417c6eaeb41
Zhiyuan Ma <631253076@qq.com>
2023-02-28 22:24:56 -0500

    leetcode 422

---
+++ 0401 - 0500/422. Valid Word Square/Solution.java
@@ -0,0 +1,20 @@
+import java.util.List;
+
+class Solution {
+    public boolean validWordSquare(List<String> words) {
+        for (int i = 0; i < words.size(); i++) {
+            for (int j = 0; j < words.get(i).length(); j++) {
+                if (j >= words.size()) {
+                    return false;
+                }
+                if (i >= words.get(j).length()) {
+                    return false;
+                }
+                if (words.get(i).charAt(j) != words.get(j).charAt(i)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
