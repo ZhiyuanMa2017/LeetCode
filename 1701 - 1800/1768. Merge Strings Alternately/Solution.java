6dd3c319a131c7bb0491f929523b1775a9adcd09
Zhiyuan Ma <631253076@qq.com>
2023-04-18 00:03:21 -0400

    leetcode 1768

---
+++ 1701 - 1800/1768. Merge Strings Alternately/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+        int j = 0;
+        while (i < word1.length() && j < word2.length()) {
+            sb.append(word1.charAt(i));
+            sb.append(word2.charAt(j));
+            i++;
+            j++;
+        }
+        while (i < word1.length()) {
+            sb.append(word1.charAt(i));
+            i++;
+        }
+        while (j < word2.length()) {
+            sb.append(word2.charAt(j));
+            j++;
+        }
+        return sb.toString();
+    }
+}
