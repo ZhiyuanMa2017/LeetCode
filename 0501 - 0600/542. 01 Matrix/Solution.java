d2d62c8f8e2d4993af28e9b2a0c166b0b0ab4a65
Zhiyuan Ma <631253076@qq.com>
2023-08-16 23:53:16 -0400

    leetcode 542

--- 0501 - 0600/542. 01 Matrix/Solution.java
+++ 0501 - 0600/542. 01 Matrix/Solution.java
@@ -1,33 +1,40 @@
-import java.util.LinkedList;
+import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Queue;
 
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
         int m = mat.length;
         int n = mat[0].length;
-        Queue<int[]> queue = new LinkedList<>();
+        int[][] res = new int[m][n];
+        Queue<int[]> queue = new ArrayDeque<>();
         for (int i = 0; i < m; i++) {
+            Arrays.fill(res[i], -1);
             for (int j = 0; j < n; j++) {
                 if (mat[i][j] == 0) {
-                    queue.add(new int[]{i, j});
-                } else {
-                    mat[i][j] = Integer.MAX_VALUE;
+                    queue.offer(new int[]{i, j, 0});
+                    res[i][j] = 0;
                 }
             }
         }
         int[] dirs = new int[]{0, 1, 0, -1, 0};
         while (!queue.isEmpty()) {
-            int[] thisCell = queue.poll();
-            for (int i = 0; i < 4; i++) {
-                int x = thisCell[0] + dirs[i];
-                int y = thisCell[1] + dirs[i + 1];
-                if (x >= 0 && y >= 0 && x < m && y < n
-                        && mat[x][y] > mat[thisCell[0]][thisCell[1]] + 1) {
-                    mat[x][y] = mat[thisCell[0]][thisCell[1]] + 1;
-                    queue.add(new int[]{x, y});
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                int[] cur = queue.poll();
+                int x = cur[0];
+                int y = cur[1];
+                int d = cur[2];
+                for (int j = 0; j < 4; j++) {
+                    int a = x + dirs[j];
+                    int b = y + dirs[j + 1];
+                    if (a >= 0 && a < m && b >= 0 && b < n && res[a][b] == -1) {
+                        res[a][b] = d + 1;
+                        queue.offer(new int[]{a, b, d + 1});
+                    }
                 }
             }
         }
-        return mat;
+        return res;
     }
 }
