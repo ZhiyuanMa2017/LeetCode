e1ef4438f80468cfc5fffd973bf35110b09cb461
Zhiyuan Ma <631253076@qq.com>
2023-10-23 20:34:53 -0400

    leetcode 515

---
+++ 0501 - 0600/515. Find Largest Value in Each Tree Row/Solution.java
@@ -0,0 +1,31 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+class Solution {
+    public List<Integer> largestValues(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            int max = Integer.MIN_VALUE;
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                max = Math.max(max, node.val);
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            res.add(max);
+        }
+        return res;
+    }
+}
