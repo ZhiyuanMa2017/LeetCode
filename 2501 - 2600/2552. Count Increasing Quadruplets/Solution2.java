b26757699e7ba18e7f922e49a01019894ce997d7
Zhiyuan Ma <631253076@qq.com>
2023-11-13 00:25:53 -0500

    leetcode 2552

--- 2501 - 2600/2552. Count Increasing Quadruplets/Solution2.java
+++ 2501 - 2600/2552. Count Increasing Quadruplets/Solution2.java
@@ -3,15 +3,18 @@ class Solution {
         int n = nums.length;
         long[] triplet = new long[n]; // 132 triplet, index is 3
         long res = 0;
-        for (int l = 0; l < n; l++) {
+        for (int r = 0; r < n; r++) {
             int cur = 0;
-            for (int j = 0; j < l; j++) {
-                if (nums[l] > nums[j]) {
-                    res += triplet[j]; // 1324, l at 4, so res += previous # of 132
-                    cur++; // count # of 1 in 132 in this round, here 2 is l and 1 is j
+            for (int j = 0; j < r; j++) {
+                if (nums[j] < nums[r]) {
+                    res += triplet[j]; // 1324, r at 4, so res += previous # of 132
+                    cur++; // count # of 1 in 132 in this round, here 2 is r and 1 is j
+                    // actually count # of 12 in this round
                 } else {
                     triplet[j] += cur; // 132, cur is # of 1 in this round,
-                                       // now find a 3, so triplet[3] += # of 1
+                    // now find a 3, so triplet[3] += #of 1
+                    // 1 3 2
+                    //   j r
                 }
             }
         }
