b9a64a172aade2b8992348b9ccac0edff3ee2f55
Zhiyuan Ma <631253076@qq.com>
2023-11-19 22:01:28 -0500

    leetcode 2115

--- 2101 - 2200/2115. Find All Possible Recipes from Given Supplies/Solution.java
+++ 2101 - 2200/2115. Find All Possible Recipes from Given Supplies/Solution.java
@@ -1,28 +1,40 @@
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Set;
+import java.util.Map;
+import java.util.Queue;
 
 class Solution {
     public List<String> findAllRecipes(String[] recipes, List<List<String>> ingredients, String[] supplies) {
-        List<String> res = new ArrayList<>();
-        int count = 0;
         int n = recipes.length;
-        Set<String> supply = new HashSet<>(Arrays.asList(supplies));
-        boolean[] find = new boolean[n];
-        while (true) {
-            for (int i = 0; i < ingredients.size(); i++) {
-                if (!find[i] && supply.containsAll(ingredients.get(i))) {
-                    res.add(recipes[i]);
-                    supply.add(recipes[i]);
-                    find[i] = true;
+        List<String> res = new ArrayList<>();
+        Map<String, List<String>> graph = new HashMap<>();
+        Map<String, Integer> inDegree = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            String recipe = recipes[i];
+            for (String ingredient : ingredients.get(i)) {
+                if (!graph.containsKey(ingredient)) {
+                    graph.put(ingredient, new ArrayList<>());
                 }
+                graph.get(ingredient).add(recipe);
+                inDegree.put(recipe, inDegree.getOrDefault(recipe, 0) + 1);
             }
-            if (count == res.size()) {
-                break;
+        }
+        Queue<String> queue = new ArrayDeque<>(Arrays.asList(supplies));
+        while (!queue.isEmpty()) {
+            String cur = queue.poll();
+            if (!graph.containsKey(cur)) {
+                continue;
             }
-            count = res.size();
+            for (String next : graph.get(cur)) {
+                if (inDegree.get(next) == 1) {
+                    queue.offer(next);
+                    res.add(next);
+                }
+                inDegree.put(next, inDegree.get(next) - 1);
+            }
         }
         return res;
     }
