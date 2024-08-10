7c2ae0143312938f5c8eb0e854a4a2a1a54e44e8
Zhiyuan Ma <631253076@qq.com>
2024-08-10 19:14:01 -0400

    leetcode 48

--- 0001 - 0100/48. Rotate Image/Solution.java
+++ 0001 - 0100/48. Rotate Image/Solution.java
@@ -1,12 +1,19 @@
 class Solution {
     public void rotate(int[][] matrix) {
         int n = matrix.length;
+//      clockwise
+//      reverse up to down, then swap the symmetry first
+//      1 2 3     7 8 9     7 4 1
+//      4 5 6  => 4 5 6  => 8 5 2
+//      7 8 9     1 2 3     9 6 3
         for (int i = 0; i < n / 2; i++) {
             int[] tmp = matrix[i];
             matrix[i] = matrix[n - i - 1];
             matrix[n - i - 1] = tmp;
         }
-
+//        7 8 9     7 4 1
+//        4 5 6  => 8 5 2
+//        1 2 3     9 6 3
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
                 int tmp = matrix[i][j];
@@ -14,5 +21,10 @@ class Solution {
                 matrix[j][i] = tmp;
             }
         }
+//      anti-clockwise,
+//      swap the symmetry first, then reverse up to down
+//      1 2 3     1 4 7     3 6 9
+//      4 5 6  => 2 5 8  => 2 5 8
+//      7 8 9     3 6 9     1 4 7
     }
 }
