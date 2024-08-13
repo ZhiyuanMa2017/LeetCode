3948f2dd1e0b4ad1675d825c3ba919475d615d24
Zhiyuan Ma <631253076@qq.com>
2024-08-13 03:02:37 -0400

    leetcode 73

--- 0001 - 0100/73. Set Matrix Zeroes/Solution.java
+++ 0001 - 0100/73. Set Matrix Zeroes/Solution.java
@@ -1,33 +1,43 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        boolean colZero = false;
-        int n = matrix.length;
-        int m = matrix[0].length;
-        for (int i = 0; i < n; i++) {
-            if (matrix[i][0] == 0) {
-                colZero = true;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean col0 = false;
+        boolean row0 = false;
+        for (int j = 0; j < n; j++) {
+            if (matrix[0][j] == 0) {
+                row0 = true;
+                break;
             }
-            for (int j = 1; j < m; j++) {
+        }
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                col0 = true;
+                break;
+            }
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
                 if (matrix[i][j] == 0) {
                     matrix[i][0] = 0;
-                    matrix[0][j] = 0;
+                    matrix[j][0] = 0;
                 }
             }
         }
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j < m; j++) {
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
                 if (matrix[i][0] == 0 || matrix[0][j] == 0) {
                     matrix[i][j] = 0;
                 }
             }
         }
-        if (matrix[0][0] == 0) {
-            for (int j = 1; j < m; j++) {
+        if (row0) {
+            for (int j = 0; j < n; j++) {
                 matrix[0][j] = 0;
             }
         }
-        if (colZero) {
-            for (int i = 0; i < n; i++) {
+        if (col0) {
+            for (int i = 0; i < m; i++) {
                 matrix[i][0] = 0;
             }
         }
