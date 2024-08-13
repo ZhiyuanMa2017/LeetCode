ba8f6ac68cd594013f5115da30c129fe182c01f2
Zhiyuan Ma <631253076@qq.com>
2024-08-13 17:24:37 -0400

    leetcode 86

--- 0001 - 0100/86. Partition List/Solution.java
+++ 0001 - 0100/86. Partition List/Solution.java
@@ -1,63 +1,24 @@
 class Solution {
     public ListNode partition(ListNode head, int x) {
-        if (head == null || head.next == null) {
-            return head;
-        }
+        ListNode lessHead = new ListNode(-1);
+        ListNode greaterHead = new ListNode(-1);
+        ListNode curLess = lessHead;
+        ListNode curGreater = greaterHead;
         ListNode cur = head;
-        ListNode lessHead = null;
-        ListNode greaterHead = null;
-        ListNode lessCur = null;
-        ListNode greaterCur = null;
-
-        while (cur != null) {
-            if (cur.val < x) {
-                if (lessHead == null) {
-                    lessHead = cur;
-                    cur = cur.next;
-                    lessHead.next = null;
-                    lessCur = lessHead;
-                } else {
-                    lessCur.next = cur;
-                    cur = cur.next;
-                    lessCur = lessCur.next;
-                    lessCur.next = null;
-                }
-            } else {
-                if (greaterHead == null) {
-                    greaterHead = cur;
-                    cur = cur.next;
-                    greaterHead.next = null;
-                    greaterCur = greaterHead;
-                } else {
-                    greaterCur.next = cur;
-                    cur = cur.next;
-                    greaterCur = greaterCur.next;
-                    greaterCur.next = null;
-                }
-            }
-        }
-        if (lessHead != null) {
-            lessCur.next = greaterHead;
-            return lessHead;
-        } else {
-            return greaterHead;
-        }
-    }
-
-    class ListNode {
-        int val;
         ListNode next;
-
-        ListNode() {
+        while (cur != null) {
+            next = cur.next;
+            cur.next = null;
+            if (cur.val < x) {
+                curLess.next = cur;
+                curLess = curLess.next;
+            } else {
+                curGreater.next = cur;
+                curGreater = curGreater.next;
+            }
+            cur = next;
         }
-
-        ListNode(int val) {
-            this.val = val;
-        }
-
-        ListNode(int val, ListNode next) {
-            this.val = val;
-            this.next = next;
-        }
+        curLess.next = greaterHead.next;
+        return lessHead.next;
     }
 }
