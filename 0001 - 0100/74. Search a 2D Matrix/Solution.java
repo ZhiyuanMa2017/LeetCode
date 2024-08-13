ce6fdd8d358703c37fb5b75ade29d3f9db10917b
Zhiyuan Ma <631253076@qq.com>
2024-08-13 03:08:34 -0400

    leetcode 74

--- 0001 - 0100/74. Search a 2D Matrix/Solution.java
+++ 0001 - 0100/74. Search a 2D Matrix/Solution.java
@@ -1,19 +1,21 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int n = matrix.length;
-        int m = matrix[0].length;
+        int m = matrix.length;
+        int n = matrix[0].length;
         int l = 0;
-        int h = n * m - 1;
-        while (l <= h) {
+        int h = m * n - 1;
+        while (l < h) {
             int mid = l + (h - l) / 2;
-            if (matrix[mid / m][mid % m] == target) {
+            int x = mid / n;
+            int y = mid % n;
+            if (matrix[x][y] == target) {
                 return true;
-            } else if (matrix[mid / m][mid % m] < target) {
+            } else if (matrix[x][y] < target) {
                 l = mid + 1;
             } else {
-                h = mid - 1;
+                h = mid;
             }
         }
-        return false;
+        return matrix[l / n][l % n] == target;
     }
 }
