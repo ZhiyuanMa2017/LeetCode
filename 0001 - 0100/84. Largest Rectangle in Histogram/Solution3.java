423198aa9ffce3a6415ffcb5e5204f4904c1935c
Zhiyuan Ma <631253076@qq.com>
2024-08-13 16:57:13 -0400

    leetcode 84

--- 0001 - 0100/84. Largest Rectangle in Histogram/Solution3.java
+++ 0001 - 0100/84. Largest Rectangle in Histogram/Solution3.java
@@ -1,23 +1,35 @@
 import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Deque;
 
 class Solution3 {
     public int largestRectangleArea(int[] heights) {
-        int res = 0;
-        Deque<Integer> s = new ArrayDeque<>();
-        s.push(-1);
-        for (int i = 0; i < heights.length; i++) {
-            while (s.peek() != -1 && heights[s.peek()] >= heights[i]) {
-                int h = heights[s.pop()];
-                int w = i - 1 - s.peek();
-                res = Math.max(res, h * w);
+        int n = heights.length;
+        int[] left = new int[n];
+        Arrays.fill(left, -1);
+        int[] right = new int[n];
+        Arrays.fill(right, n);
+        Deque<Integer> deque = new ArrayDeque<Integer>();
+        for (int i = 0; i < n; i++) {
+            while (!deque.isEmpty() && heights[deque.peek()] > heights[i]) {
+                right[deque.poll()] = i;
             }
-            s.push(i);
+            deque.push(i);
         }
-        while (s.peek() != -1) {
-            int h = heights[s.pop()];
-            int w = heights.length - 1 - s.peek();
-            res = Math.max(res, h * w);
+        deque = new ArrayDeque<>();
+        for (int i = n - 1; i >= 0; i--) {
+            while (!deque.isEmpty() && heights[deque.peek()] > heights[i]) {
+                left[deque.poll()] = i;
+            }
+            deque.push(i);
+        }
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            int h = heights[i];
+            int l = left[i];
+            int r = right[i];
+            int w = r - l - 1;
+            res = Math.max(res, w * h);
         }
         return res;
     }
