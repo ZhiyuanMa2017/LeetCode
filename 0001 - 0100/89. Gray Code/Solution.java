74f55bd8abf586fcd8904aedfdf6307bd57ce498
Zhiyuan Ma <631253076@qq.com>
2024-08-14 02:32:43 -0400

    leetcode 89

--- 0001 - 0100/89. Gray Code/Solution.java
+++ 0001 - 0100/89. Gray Code/Solution.java
@@ -5,6 +5,11 @@ class Solution {
     public List<Integer> grayCode(int n) {
         List<Integer> res = new ArrayList<>();
         res.add(0);
+        // 00 01
+        // 11 10
+        // 00 01 11 10
+        // 00   01  11  10  10  11  01  00
+        // 000 001 011 010 110 111 101 100
         for (int i = 0; i < n; i++) {
             int s = res.size();
             for (int j = s - 1; j >= 0; j--) {
