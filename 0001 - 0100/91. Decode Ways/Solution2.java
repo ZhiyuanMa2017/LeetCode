5e1f28a9e7d4c547a4545aef49bf7824c8a935b2
Zhiyuan Ma <631253076@qq.com>
2024-08-14 02:49:33 -0400

    leetcode 91

--- 0001 - 0100/91. Decode Ways/Solution2.java
+++ 0001 - 0100/91. Decode Ways/Solution2.java
@@ -1,24 +1,21 @@
-class Solution {
+class Solution2 {
     public int numDecodings(String s) {
         int n = s.length();
-        int[] dp = new int[n];
-        if (s.charAt(0) == '0') {
-            return 0;
-        }
+        s = " " + s;
+        int[] dp = new int[n + 1];
         dp[0] = 1;
-        for (int i = 1; i < s.length(); i++) {
-            if (s.charAt(i) != '0') {
-                dp[i] = dp[i - 1];
+        for (int i = 1; i <= n; i++) {
+            char c = s.charAt(i);
+            if (c != '0') {
+                dp[i] += dp[i - 1];
             }
-            int two = Integer.parseInt(s.substring(i - 1, i + 1));
-            if (two >= 10 && two <= 26) {
-                if (i > 2) {
+            if (i > 1) {
+                int num = Integer.parseInt(s.substring(i - 1, i + 1));
+                if (num >= 10 && num <= 26) {
                     dp[i] += dp[i - 2];
-                } else {
-                    dp[i] += 1;
                 }
             }
         }
-        return dp[n - 1];
+        return dp[n];
     }
 }
