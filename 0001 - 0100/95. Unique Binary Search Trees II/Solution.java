e75476a3a6befa8c27e778044658ed6dc6168480
Zhiyuan Ma <631253076@qq.com>
2024-08-14 03:37:16 -0400

    leetcode 95

---
+++ 0001 - 0100/95. Unique Binary Search Trees II/Solution.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<TreeNode> generateTrees(int n) {
+        return dfs(1, n);
+    }
+
+    private List<TreeNode> dfs(int start, int end) {
+        List<TreeNode> res = new ArrayList<>();
+        if (start > end) {
+            res.add(null);
+            return res;
+        }
+        for (int i = start; i <= end; i++) {
+            List<TreeNode> left = dfs(start, i - 1);
+            List<TreeNode> right = dfs(i + 1, end);
+            for (TreeNode l : left) {
+                for (TreeNode r : right) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = l;
+                    root.right = r;
+                    res.add(root);
+                }
+            }
+        }
+        return res;
+    }
+}
