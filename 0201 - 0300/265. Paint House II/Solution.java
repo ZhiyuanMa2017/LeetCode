acc45b6b60f3f8b0f0234fe05980e7625e12f2f7
Zhiyuan Ma <631253076@qq.com>
2024-10-29 00:52:19 -0400

    leetcode 265

--- 0201 - 0300/265. Paint House II/Solution.java
+++ 0201 - 0300/265. Paint House II/Solution.java
@@ -1,43 +1,23 @@
 class Solution {
     public int minCostII(int[][] costs) {
-        int[] res = costs[0];
-        int k = costs[0].length;
         int n = costs.length;
-
+        int k = costs[0].length;
         for (int i = 1; i < n; i++) {
-            int minColor = -1;
-            int secondMinColor = -1;
             for (int j = 0; j < k; j++) {
-                int cost = res[j];
-                if (minColor == -1 || cost < res[minColor]) {
-                    secondMinColor = minColor;
-                    minColor = j;
-                } else if (secondMinColor == -1 || cost < res[secondMinColor]) {
-                    secondMinColor = j;
+                int min = Integer.MAX_VALUE;
+                for (int p = 0; p < k; p++) {
+                    if (p == j) {
+                        continue;
+                    }
+                    min = Math.min(min, costs[i - 1][p]);
                 }
+                costs[i][j] += min;
             }
-            int[] temp = new int[costs[0].length];
-            for (int color = 0; color < k; color++) {
-                if (color == minColor) {
-                    temp[color] = res[secondMinColor] + costs[i][color];
-                } else {
-                    temp[color] = res[minColor] + costs[i][color];
-                }
-            }
-            res = temp;
         }
-        int min = Integer.MAX_VALUE;
-        for (int i = 0; i < res.length; i++) {
-            min = Math.min(min, res[i]);
+        int res = Integer.MAX_VALUE;
+        for (int j = 0; j < k; j++) {
+            res = Math.min(res, costs[n - 1][j]);
         }
-        return min;
-    }
-
-    public static void main(String[] args) {
-        Solution s = new Solution();
-        System.out.println(s.minCostII(new int[][]{
-                {10, 15, 12, 14, 18, 5}, {5, 12, 18, 13, 15, 8},
-                {4, 7, 4, 2, 10, 18}, {20, 9, 9, 19, 20, 5}, {10, 15, 10, 15, 16, 20},
-                {9, 6, 11, 10, 12, 11}, {7, 10, 6, 12, 20, 8}, {3, 4, 4, 18, 10, 2}}));
+        return res;
     }
 }
