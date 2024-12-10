710c898b8dd0f091cb25859043adf04c4c1b1eb2
Zhiyuan Ma <631253076@qq.com>
2024-12-09 23:46:40 -0500

    leetcode 287

--- 0201 - 0300/287. Find the Duplicate Number/Solution.java
+++ 0201 - 0300/287. Find the Duplicate Number/Solution.java
@@ -2,7 +2,12 @@ class Solution {
     public int findDuplicate(int[] nums) {
         int slow = nums[0];
         int fast = nums[0];
-
+//        slow = F + a
+//        fast = 2(F + a) = F + a + nC
+//        F + a = nC
+//        a = nC - F in circle
+//        fast is F away from the starting node
+//        leetcode 142
         while (true) {
             slow = nums[slow];
             fast = nums[nums[fast]];
